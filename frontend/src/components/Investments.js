fb8275f41acd19e5356a16f9037343222f9c90dd
Asleshakan <142888325+Asleshakan@users.noreply.github.com>
2025-01-28 17:55:57 +0530

    Update Investments.js

--- frontend/src/components/Investments.js
+++ frontend/src/components/Investments.js
@@ -6,7 +6,7 @@ const Investments = () => {
 
   useEffect(() => {
     axios
-      .get("https://kareactnodeapp-g6h8dxh9dxdrhubc.canadacentral-01.azurewebsites.net/investments")
+      .get("http://localhost:5000/investments")
       .then((response) => setData(response.data))
       .catch((error) => console.error("Error fetching investments data:", error));
   }, []);
